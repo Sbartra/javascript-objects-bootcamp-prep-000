7aecb7f16e895e8a019506c3fd6526a4954fe8aa
Segundo Bartra <segundobartra@gmail.com>
2019-03-03 19:55:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   "name": "maddona",
   "track": "Isla Bonita"
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
